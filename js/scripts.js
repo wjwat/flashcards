d2c0dee9a2245ab6e662c5141c214db9767b9741
William Watkins <wjwat@onionslice.org>
2022-01-11 15:52:42 -0600

    allow tighter selection of cards & correct parent
    Co-authored-by: Mikah Mathews <mikah@outofband.org>

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
 
   $(".card-title").click(function() {
 		$(this).siblings('.card-text').toggleClass('our-def');
+		$(this).parent().toggleClass('card-new-bg');
   });
 });
